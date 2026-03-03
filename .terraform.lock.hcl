d28dbfa0074af37772e30e59e8548dab36925656
Jacob Rickey <jr521816@ohio.edu>
2026-03-02 23:46:18 -0500

    Initial commit: Azure infrastructure and Ansible baseline

---
+++ .terraform.lock.hcl
@@ -0,0 +1,40 @@
+# This file is maintained automatically by "tofu init".
+# Manual edits may be lost in future updates.
+
+provider "registry.opentofu.org/hashicorp/azurerm" {
+  version = "4.62.1"
+  hashes = [
+    "h1:fCdD5upUmvvjb0Z/Fvppfl3acJWzD1eMk+/JB2LQ1Hg=",
+    "zh:012505b97946671239d7c778eeff470f21afb0b2b1cc3f2df26199351593b865",
+    "zh:31eca20af47e251e135bcf8d6797fc04e9cb61560ee622495eaa32e6d9e24a44",
+    "zh:4d27eada9ee55a93e23abd5604d445bf074cdb32ed2e60067e15475f23d2e67e",
+    "zh:5d53799176e9f2bbcf913bf0329fbcdd0b173821c21cbbabfcd86e68a7641b3a",
+    "zh:941f1ed72c6fb6d5adccc77535db5e543d0abab7bffc7aff04d06d1b760f499c",
+    "zh:9c79bcdbb04dd2c00f12492775a007101ae2fc43f7bce64ebd22c365686f8278",
+    "zh:9d903b4ea69bc3423100bb3dbfdd4620303036fefb757f0cc2b0759729c40bfc",
+    "zh:a389501070e30aec75a39f2b6bcffff30bc11a4656737653494ec1943fe49b8a",
+    "zh:a98b69e5ce99e16dcaa1720f37b81bef0ee2178f79af399a53eeb0e4bca331c3",
+    "zh:a9e17d9c5161d07a3b36bcc6d10bf50e72d48b3d5c13297f580914526d3b9698",
+    "zh:ba6b749f586efafd045b53ed28842f53fb6e5549bd34f3774adced43134938b6",
+    "zh:bb3b4e0951517bed40ea015431c694a19311f9b5c5440b0b38a80c297dd1df12",
+    "zh:c50bf94a3ea3129369941cc04628254039fbf1156cdffb1ee5af79156e3fd51e",
+    "zh:db29d958158f6d5699dbb8d35d641c52fb56f3db74df8c884cc79652719e70da",
+    "zh:df105d3cd116f7dd42fa1b463871f5e90c99533b6592d1bf1e9d9c825286098f",
+  ]
+}
+
+provider "registry.opentofu.org/hashicorp/local" {
+  version = "2.7.0"
+  hashes = [
+    "h1:bKZhLyajpK7pEizfT3rOpfhUl+gLSQviaX7cYat3WB0=",
+    "zh:39e037a963356e583d90d509d82f6dc19914ef5c66970fb166db414f035468f4",
+    "zh:5292e51488d40d6c2b365daa9a406144c3fa3f769f1c03065adb4757d41c6ea0",
+    "zh:62db48adf8676e8c67f923352a4acb8e52470220ecaa0c9e21a660f359fd5446",
+    "zh:6d5f4555371edde0975b5c2ce5fb048be737ea5dc9aab75c8f9fe37f37bf7850",
+    "zh:790ab029516ee126a2b5a122ab0638c09585c71c109b91cefc794a4ecc2ba32e",
+    "zh:7b7410b923c17a3495e416b940dbef7ee6e2e82298ea2f5b7f9a0e4c2cad4b69",
+    "zh:8baa1caf36ba2b0b63e91cd00750e643d21f13535dce04ae824a1211537c6867",
+    "zh:aebc221a0da83e970c737c71e76701df731c6f8d70e56ead85bc1f83996f852d",
+    "zh:b3c3ee356591800b11d45fb0bb7d39c8eb3a2141c56dd87808b1fcdc9380816c",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,194 @@
+﻿provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "cloud-v3"
+  location = "southcentralus"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "cloud-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "app_subnet" {
+  name                 = "app-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "db_subnet" {
+  name                 = "db-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "app_pip" {
+  name                = "app-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_security_group" "app_nsg" {
+  name                = "app-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "75.188.18.74/32"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_security_group" "db_nsg" {
+  name                = "db-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowMySQL"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3306"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "10.0.1.0/24"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "app_nic" {
+  name                = "app-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.app_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.app_pip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "app_nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.app_nic.id
+  network_security_group_id = azurerm_network_security_group.app_nsg.id
+}
+
+resource "azurerm_network_interface" "db_nic" {
+  name                = "db-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.db_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "db_nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.db_nic.id
+  network_security_group_id = azurerm_network_security_group.db_nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "app_vm" {
+  name                = "app-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2pts_v2"
+  admin_username      = "jrick"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "jrick"
+    public_key = file("mits_key.pub")
+  }
+
+  network_interface_ids = [azurerm_network_interface.app_nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server-arm64"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "db_vm" {
+  name                = "db-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2pts_v2"
+  admin_username      = "jrick"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "jrick"
+    public_key = file("mits_key.pub")
+  }
+
+  network_interface_ids = [azurerm_network_interface.db_nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server-arm64"
+    version   = "latest"
+  }
+}
+
+resource "local_file" "ansible_inventory" {
+  content = templatefile("inventory.tmpl", {
+    public_ip = azurerm_public_ip.app_pip.ip_address
+  })
+  filename = "inventory.ini"
+}
