3481070d4f9deb4df71ad615319ad60f82b8f400
dr-professor-jacob <jr521816@ohio.edu>
2026-04-05 06:38:05 -0400

    Remove Azure project link; fix outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,8 +13,3 @@ output "app_public_ip" {
   value       = azurerm_public_ip.app_pip.ip_address
 }
 
-output "db_private_ip" {
-  description = "Private IP address of the DB VM"
-  value       = azurerm_network_interface.db_nic.private_ip_address
-}
-
